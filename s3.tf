9f9df1f05baaf2658d5204e49730e9ed9da4fb5c
hd962q <77110448+hd962q@users.noreply.github.com>
2021-01-15 14:44:15 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -4,6 +4,13 @@ resource "aws_s3_bucket" "b" {
   versioning {
     enabled = true
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
   tags = {
     Name        = "My bucket"
   }
