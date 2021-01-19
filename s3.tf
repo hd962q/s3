7424c36a46ff6ca55370dbf3009ea610a30459d8
hd962q <77110448+hd962q@users.noreply.github.com>
2021-01-18 19:09:50 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 provider "aws" {
  region = "us-east-2"
 }
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "terraform-bucket" {
   bucket = "my-tf-test-bucket"
   acl    = "private"
   versioning {
